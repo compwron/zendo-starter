0ff13e74571b955d4342e241f0f7815d165dd469
Linda Goldstein <lindag@squareup.com>
2018-06-21 17:54:45 -0700

    Ruby framework

---
+++ zendo.rb
@@ -0,0 +1,11 @@
+colors = %w[red blue green]
+sizes = %w[small medium large]
+
+def evaluate_play(rule, play)
+#   your code here
+end
+
+rule = '1 large red'
+play = '1 large red'
+
+p evaluate_play(rule, play)
